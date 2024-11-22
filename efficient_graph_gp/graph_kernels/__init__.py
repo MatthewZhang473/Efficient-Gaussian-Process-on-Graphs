f16c317dd2bb2cb19eb56d55c03bb71549a33baa
Matthew_Zhang_Mac <mz473@cam.ac.uk>
2024-11-22 20:49:23 +0000

    Implemented the product-of-feature-matrix kernel, tested the convergence to ground truth.

--- efficient_graph_gp/graph_kernels/__init__.py
+++ efficient_graph_gp/graph_kernels/__init__.py
@@ -1,8 +1,10 @@
 from .diffusion_kernel import diffusion_kernel
+from .feature_matrix_kernel import feature_matrix_kernel
 from .utils import get_normalized_laplacian, generate_noisy_samples
 
 __all__ = [
             "get_normalized_laplacian",
             "generate_noisy_samples",
-            "diffusion_kernel"
+            "diffusion_kernel",
+            "feature_matrix_kernel"
           ]
