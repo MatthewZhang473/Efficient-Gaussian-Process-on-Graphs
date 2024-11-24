739cb3ea5ee0190da497b74b00e6ff0c7ce394e2
Matthew_Zhang_Mac <mz473@cam.ac.uk>
2024-11-24 14:41:23 +0000

    added the GRF_diffusion_kernel to __init__.py

--- efficient_graph_gp/gpflow_kernels/__init__.py
+++ efficient_graph_gp/gpflow_kernels/__init__.py
@@ -1,7 +1,9 @@
 from .diffusion_kernel_exact import GraphDiffusionKernel
 from .diffusion_kernel_pofm import GraphDiffusionPoFMKernel
+from .diffusion_kernel_grf import GraphDiffusionGRFKernel
 
 __all__ = [
             "GraphDiffusionKernel",
-            "GraphDiffusionPoFMKernel"
+            "GraphDiffusionPoFMKernel",
+            "GraphDiffusionGRFKernel"
           ]
