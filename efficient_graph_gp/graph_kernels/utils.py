d103dba212a0da91a8bab881f464417ebf1b462e
Matthew_Zhang_Mac <mz473@cam.ac.uk>
2024-12-18 18:56:36 +0000

    Added sparse matrix support for get_normalized_laplacian

--- efficient_graph_gp/graph_kernels/utils.py
+++ efficient_graph_gp/graph_kernels/utils.py
@@ -1,20 +1,30 @@
 import numpy as np
+import scipy.sparse as sp
 
-def get_normalized_laplacian(W):
+#TODO: Test the correctness of the sparse implementation of get_normalized_laplacian
+
+def get_normalized_laplacian(W, sparse=False):
     """
     Compute the normalized Laplacian matrix for a graph.
 
     Parameters:
-    W (ndarray): Symmetric adjacency matrix of shape (n, n).
+    W (ndarray or sparse matrix): Symmetric adjacency matrix of shape (n, n).
+    sparse (bool): Flag indicating whether the input graph is sparse. Default is False.
 
     Returns:
-    ndarray: Normalized Laplacian matrix of shape (n, n).
+    ndarray or sparse matrix: Normalized Laplacian matrix of shape (n, n).
     """
-    degrees = np.sum(W, axis=1)
-    D_inv_sqrt = np.zeros_like(W, dtype=float)
-    valid_degrees = degrees > 0
-    D_inv_sqrt[valid_degrees, valid_degrees] = 1.0 / np.sqrt(degrees[valid_degrees])
-    L = np.eye(W.shape[0]) - D_inv_sqrt @ W @ D_inv_sqrt
+    if sparse:
+        degrees = np.array(W.sum(axis=1)).flatten()
+        D_inv_sqrt = sp.diags([1.0 / np.sqrt(degrees[i]) if degrees[i] > 0 else 0.0 for i in range(len(degrees))])
+        L = sp.eye(W.shape[0]) - D_inv_sqrt @ W @ D_inv_sqrt
+    else:
+        degrees = np.sum(W, axis=1)
+        D_inv_sqrt = np.zeros_like(W, dtype=float)
+        valid_degrees = degrees > 0
+        D_inv_sqrt[valid_degrees, valid_degrees] = 1.0 / np.sqrt(degrees[valid_degrees])
+        L = np.eye(W.shape[0]) - D_inv_sqrt @ W @ D_inv_sqrt
+
     return L
 
 def generate_noisy_samples(K, noise_std=0.1):
