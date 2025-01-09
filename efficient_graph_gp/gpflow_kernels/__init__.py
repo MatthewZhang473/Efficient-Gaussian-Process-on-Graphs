d0fcbc5b5d706debbe933bc7b1288c9395c6afc8
Matthew_Zhang_Mac <mz473@cam.ac.uk>
2025-01-09 15:59:28 +0000

    Implemented an fast implementation of the GraphGeneralPoFMKernel, which uses the PoFM-based kernel to learn arbitrary modulator vector.

--- efficient_graph_gp/gpflow_kernels/__init__.py
+++ efficient_graph_gp/gpflow_kernels/__init__.py
@@ -2,10 +2,12 @@ from .diffusion_kernel_exact import GraphDiffusionKernel
 from .diffusion_kernel_pofm import GraphDiffusionPoFMKernel
 from .diffusion_kernel_grf import GraphDiffusionGRFKernel
 from .diffusion_kernel_fast_grf import GraphDiffusionFastGRFKernel
+from .general_kernel_pofm import GraphGeneralPoFMKernel
 
 __all__ = [
             "GraphDiffusionKernel",
             "GraphDiffusionPoFMKernel",
             "GraphDiffusionGRFKernel",
-            "GraphDiffusionFastGRFKernel"
+            "GraphDiffusionFastGRFKernel",
+            "GraphGeneralPoFMKernel"
           ]
