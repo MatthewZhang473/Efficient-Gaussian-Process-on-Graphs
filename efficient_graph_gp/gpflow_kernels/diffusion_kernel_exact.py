086d5d014baae566bf64da8baf6f21823d1d2c61
Matthew_Zhang_Mac <mz473@cam.ac.uk>
2025-02-04 16:23:38 +0000

    Improved flexibility of the diffusion kernel:
    1. Added important hyperparam sigma_f to control the kernel variance.
    
    2. Added option to normalize / not normalize the laplacian.

--- efficient_graph_gp/gpflow_kernels/diffusion_kernel_exact.py
+++ efficient_graph_gp/gpflow_kernels/diffusion_kernel_exact.py
@@ -4,30 +4,38 @@ import numpy as np
 
 
 class GraphDiffusionKernel(gpflow.kernels.Kernel):
-    def __init__(self, adjacency_matrix, beta = None, **kwargs):
+    def __init__(self, adjacency_matrix, beta = None, sigma_f = None, normalize_laplacian = True,**kwargs):
         super().__init__(**kwargs)
         self.adjacency_matrix = tf.convert_to_tensor(adjacency_matrix, dtype=tf.float64)
-        if beta:
-            self.beta = gpflow.Parameter(beta, transform=gpflow.utilities.positive())
-        else:
-            self.beta = gpflow.Parameter(2.0, transform=gpflow.utilities.positive())  # Learnable hyperparameter
+        self.normalize_laplacian = normalize_laplacian
+        self.beta = gpflow.Parameter(beta or 2.0, transform=gpflow.utilities.positive())
+        self.sigma_f = gpflow.Parameter(sigma_f or 1.0, transform=gpflow.utilities.positive())
 
     def K(self, X1, X2=None):
         if X2 is None:
             X2 = X1
-        kernel_matrix = self.diffusion_kernel(self.adjacency_matrix, self.beta)
+        kernel_matrix = self.diffusion_kernel(self.adjacency_matrix, self.beta, self.sigma_f)
         indices_X1 = tf.cast(tf.reshape(X1, [-1]), dtype=tf.int32)
         indices_X2 = tf.cast(tf.reshape(X2, [-1]), dtype=tf.int32)
         return tf.gather(tf.gather(kernel_matrix, indices_X1, axis=0), indices_X2, axis=1)
 
     def K_diag(self, X):
-        kernel_matrix = self.diffusion_kernel(self.adjacency_matrix, self.beta)
+        kernel_matrix = self.diffusion_kernel(self.adjacency_matrix, self.beta, self.sigma_f)
         indices_X = tf.cast(tf.reshape(X, [-1]), dtype=tf.int32)
         return tf.gather(tf.linalg.diag_part(kernel_matrix), indices_X)
 
-    def diffusion_kernel(self, adj_matrix, beta):
-        normalized_laplacian = self.get_normalized_laplacian(adj_matrix)
-        return tf.linalg.expm(-beta * normalized_laplacian)
+    def diffusion_kernel(self, adj_matrix, beta, sigma_f):
+        if self.normalize_laplacian:
+            normalized_laplacian = self.get_normalized_laplacian(adj_matrix)
+            return sigma_f ** 2 * tf.linalg.expm(-beta * normalized_laplacian)
+        else:
+            laplacian = self.get_laplacian(adj_matrix)
+            return sigma_f ** 2 * tf.linalg.expm(-beta * laplacian)
+        
+    @staticmethod
+    def get_laplacian(adj_matrix):
+        D = tf.linalg.diag(tf.reduce_sum(adj_matrix, axis=1))
+        return D - adj_matrix
     
     @staticmethod
     def get_normalized_laplacian(adj_matrix):
