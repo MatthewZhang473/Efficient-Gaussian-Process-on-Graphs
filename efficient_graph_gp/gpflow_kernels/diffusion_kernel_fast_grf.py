06d0c4471d3fcc63408e5829b0ccf8084aae3ee8
Matthew_Zhang_Mac <mz473@cam.ac.uk>
2025-02-10 12:45:19 +0000

    Improved the GRF/PoFM based diffusion kernel with 1. sigma_f paramterization for controlling the kernel variance 2. options to use the un-normalized laplacian kernel

--- efficient_graph_gp/gpflow_kernels/diffusion_kernel_fast_grf.py
+++ efficient_graph_gp/gpflow_kernels/diffusion_kernel_fast_grf.py
@@ -2,7 +2,7 @@ import gpflow
 import tensorflow as tf
 from ..random_walk_samplers import Graph, RandomWalk
 from ..modulation_functions import diffusion_modulator_tf
-from ..preprocessing import get_normalized_laplacian
+from ..preprocessing import get_normalized_laplacian, get_laplacian
 
 
 class GraphDiffusionFastGRFKernel(gpflow.kernels.Kernel):
@@ -12,8 +12,10 @@ class GraphDiffusionFastGRFKernel(gpflow.kernels.Kernel):
         walks_per_node: int = 50,
         p_halt: float = 0.1,
         max_walk_length: int = 10,
-        beta: float = 1.0, # this is the learnable parameter
+        beta: float = 2.0, # this is the learnable parameter
+        sigma_f: float = 1.0,
         random_walk_seed: int = 42,
+        normalize_laplacian: bool = True,
         **kwargs
     ):
         super().__init__(**kwargs)
@@ -24,9 +26,10 @@ class GraphDiffusionFastGRFKernel(gpflow.kernels.Kernel):
         self.p_halt = p_halt
         self.max_walk_length = max_walk_length
         self.beta = gpflow.Parameter(tf.cast(beta, tf.float64), transform=gpflow.utilities.positive())
+        self.sigma_f = gpflow.Parameter(tf.cast(sigma_f, tf.float64), transform=gpflow.utilities.positive())
         
         # Precompute random walk feature matrices
-        self.laplacian = get_normalized_laplacian(adjacency_matrix)
+        self.laplacian = get_normalized_laplacian(self.adjacency_matrix) if normalize_laplacian else get_laplacian(self.adjacency_matrix)
         graph = Graph(self.laplacian)
         random_walk = RandomWalk(graph, seed=random_walk_seed)
         self.feature_matrices_tf = tf.constant(
@@ -36,21 +39,21 @@ class GraphDiffusionFastGRFKernel(gpflow.kernels.Kernel):
     def K(self, X1: tf.Tensor, X2: tf.Tensor = None) -> tf.Tensor:
         if X2 is None:
             X2 = X1
-        kernel_matrix = self.grf_kernel(self.beta)
+        kernel_matrix = self.grf_kernel(self.beta, self.sigma_f)
         indices_X1 = tf.cast(tf.reshape(X1, [-1]), dtype=tf.int32)
         indices_X2 = tf.cast(tf.reshape(X2, [-1]), dtype=tf.int32)
         return tf.gather(tf.gather(kernel_matrix, indices_X1, axis=0), indices_X2, axis=1)
 
     def K_diag(self, X: tf.Tensor) -> tf.Tensor:
-        kernel_matrix = self.grf_kernel(self.beta)
+        kernel_matrix = self.grf_kernel(self.beta, self.sigma_f)
         indices_X = tf.cast(tf.reshape(X, [-1]), dtype=tf.int32)
         return tf.gather(tf.linalg.diag_part(kernel_matrix), indices_X)
 
-    def grf_kernel(self, beta: tf.Tensor) -> tf.Tensor:
+    def grf_kernel(self, beta: tf.Tensor, sigma_f) -> tf.Tensor:
         # Compute the modulator vector
         modulator_vector = diffusion_modulator_tf(
             tf.range(self.max_walk_length, dtype=tf.float64), tf.cast(beta, tf.float64)
         )
         # Ensure consistency in data types
         Phi = tf.linalg.matmul(self.feature_matrices_tf, modulator_vector[:, tf.newaxis])[:, :, 0]
-        return tf.matmul(Phi, tf.transpose(Phi))
+        return sigma_f**2 * tf.matmul(Phi, tf.transpose(Phi))
--- efficient_graph_gp/gpflow_kernels/diffusion_kernel_pofm.py
+++ efficient_graph_gp/gpflow_kernels/diffusion_kernel_pofm.py
@@ -2,14 +2,16 @@ import gpflow
 import tensorflow as tf
 from math import factorial
 import numpy as np
-
+from ..preprocessing import get_normalized_laplacian, get_laplacian
 
 class GraphDiffusionPoFMKernel(gpflow.kernels.Kernel):
-    def __init__(self, adjacency_matrix, max_expansion=5, **kwargs):
+    def __init__(self, adjacency_matrix, max_expansion=5, sigma_f=1.0, normalize_laplacian=True, **kwargs):
         super().__init__(**kwargs)
         self.adjacency_matrix = tf.convert_to_tensor(adjacency_matrix, dtype=tf.float64)
         self.beta = gpflow.Parameter(2.0, transform=gpflow.utilities.positive())  # Learnable hyperparameter
         self.max_expansion = max_expansion  # Maximum expansion for series approximation
+        self.sigma_f = gpflow.Parameter(sigma_f, transform=gpflow.utilities.positive())  # Learnable hyperparameter
+        self.normalize_laplacian = normalize_laplacian
 
     def K(self, X1, X2=None):
         if X2 is None:
@@ -25,21 +27,14 @@ class GraphDiffusionPoFMKernel(gpflow.kernels.Kernel):
         return tf.gather(tf.linalg.diag_part(kernel_matrix), indices_X)
 
     def compute_diffusion_kernel(self, adj_matrix, beta, max_expansion):
-        laplacian = self.get_normalized_laplacian(adj_matrix)
+        
+        laplacian = get_normalized_laplacian(adj_matrix) if self.normalize_laplacian else get_laplacian(adj_matrix)
         modulation_coefficients = [(-beta / 2) ** i / factorial(i) for i in range(max_expansion + 1)]
         L_basis = [tf.eye(tf.shape(laplacian)[0], dtype=laplacian.dtype)]
         for _ in range(1, max_expansion + 1):
             L_basis.append(tf.matmul(L_basis[-1], laplacian))
         K_f = sum(coeff * L for coeff, L in zip(modulation_coefficients, L_basis))
-        return tf.matmul(K_f, tf.transpose(K_f))  # Ensure symmetry of the kernel
-
-    @staticmethod
-    def get_normalized_laplacian(adj_matrix):
-        degrees = tf.reduce_sum(adj_matrix, axis=1)
-        safe_degrees = tf.where(degrees > 0, degrees, tf.constant(float('inf'), dtype=adj_matrix.dtype))
-        D_inv_sqrt = tf.linalg.diag(1.0 / tf.sqrt(safe_degrees))
-        I = tf.eye(tf.shape(adj_matrix)[0], dtype=adj_matrix.dtype)
-        return I - tf.matmul(tf.matmul(D_inv_sqrt, adj_matrix), D_inv_sqrt)
+        return self.sigma_f**2 * tf.matmul(K_f, tf.transpose(K_f))  # Ensure symmetry of the kernel
     
 if __name__ == "__main__":
 
