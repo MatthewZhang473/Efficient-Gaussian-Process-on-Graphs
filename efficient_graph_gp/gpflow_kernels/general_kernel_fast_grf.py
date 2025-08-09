c98cb191f8d9a1a8a93573cad1521ce2425d9586
matthew_cbl <mz473@cam.ac.uk>
2025-08-09 23:56:52 +0100

    Implemented large scale timing experiments

--- efficient_graph_gp/gpflow_kernels/general_kernel_fast_grf.py
+++ efficient_graph_gp/gpflow_kernels/general_kernel_fast_grf.py
@@ -15,6 +15,7 @@ class GraphGeneralFastGRFKernel(gpflow.kernels.Kernel):
         max_walk_length: int = 10,
         random_walk_seed: int = 42,
         modulator_vector: np.ndarray = None,
+        step_matrices: np.ndarray = None,  # NEW: pre-computed step matrices
         use_tqdm: bool = False,
         **kwargs
     ):
@@ -30,22 +31,25 @@ class GraphGeneralFastGRFKernel(gpflow.kernels.Kernel):
             # initialize the modulator vector as a random vector, learnable
             np.random.seed(42)
             initial_modulator_vector = np.random.randn(max_walk_length)
-            self.modulator_vector =  gpflow.Parameter(tf.cast(initial_modulator_vector, tf.float64)) #TODO: Check Initialization & Constraint
+            self.modulator_vector = gpflow.Parameter(tf.cast(initial_modulator_vector, tf.float64))
         else:
             # initialize the modulator vector as a given vector
             if len(modulator_vector) != max_walk_length:
                 raise ValueError("The length of the modulator vector must be equal to the max_walk_length.")
             else:
-                self.modulator_vector =  gpflow.Parameter(tf.cast(modulator_vector, tf.float64))
+                self.modulator_vector = gpflow.Parameter(tf.cast(modulator_vector, tf.float64))
         
-        
-        # Precompute random walk feature matrices
-        self.laplacian = get_normalized_laplacian(adjacency_matrix)
-        graph = Graph(self.laplacian)
-        random_walk = RandomWalk(graph, seed=random_walk_seed)
-        self.feature_matrices_tf = tf.constant(
-            random_walk.get_random_walk_matrices(walks_per_node, p_halt, max_walk_length, use_tqdm=use_tqdm), dtype=tf.float64
-        )
+        # Handle step matrices - either load pre-computed or compute new ones
+        if step_matrices is not None:
+            self.feature_matrices_tf = tf.constant(step_matrices, dtype=tf.float64)
+        else:
+            # Precompute random walk feature matrices
+            self.laplacian = get_normalized_laplacian(adjacency_matrix)
+            graph = Graph(self.laplacian)
+            random_walk = RandomWalk(graph, seed=random_walk_seed)
+            self.feature_matrices_tf = tf.constant(
+                random_walk.get_random_walk_matrices(walks_per_node, p_halt, max_walk_length, use_tqdm=use_tqdm), dtype=tf.float64
+            )
 
     def K(self, X1: tf.Tensor, X2: tf.Tensor = None) -> tf.Tensor:
         if X2 is None:
